36dbc79519c8919d0c8f5d05e595fce1430ddcd2
batmanov <batman@mail.ru>
2020-08-25 16:27:17 +0300

    ref conversion

--- app/controllers/conversion_credit_controller.rb
+++ app/controllers/conversion_credit_controller.rb
@@ -5,7 +5,7 @@ class ConversionCreditController < ApplicationController
   def calculate
     if params[:in].present? && params[:out].present?
       conversion_calculator = ConversionCalculator.new(params[:in].split(","), params[:out].split(","))
-      flash[:notice] = conversion_calculator.conversion
+      flash[:notice] = "Конверсия #{conversion_calculator.conversion}"
       redirect_to action: :index
     else
       flash[:error] = "неверный формат данных"
